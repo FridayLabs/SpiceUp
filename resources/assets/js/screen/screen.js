1335e953ff34391fcf70a1dea65704c53611b461
Vladimir Panov <vpanov@dealersocket.com>
2017-02-18 14:53:12 +0200

    #001 init timer widget

--- resources/assets/js/screen/screen.js
+++ resources/assets/js/screen/screen.js
@@ -1,6 +1,7 @@
 window.Vue = require('vue');
 
 Vue.component('test', require('./widgets/Test.vue'));
+Vue.component('timer', require('./widgets/Timer.vue'));
 
 const app = new Vue({
 	el: '#app'
--- resources/views/test_screen.blade.php
+++ resources/views/test_screen.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <test></test>
+	<timer></timer>
 @endsection
