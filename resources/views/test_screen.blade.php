b9034e08485136f44977f46f9aebdb632aba033d
Vladimir Panov <vpanov@dealersocket.com>
2017-02-19 15:09:05 +0200

    #010 change data

--- resources/views/test_screen.blade.php
+++ resources/views/test_screen.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.screen')
 
 @section('content')
-	<head-panel v-bind:score-data="scoreData" v-bind:timer-data="timerData"></head-panel>
+	<head-panel
+			v-bind:team-data="teamData"
+			v-bind:timer-data="timerData"
+			v-bind:score-data="scoreData"
+			v-bind:substitution-data="substitutionData"></head-panel>
 @endsection
